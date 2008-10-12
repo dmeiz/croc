8f9a9cf42fa88d4410d9a478be39bcd98be1205b
Dan Hensgen <dan@macbook.(none)>
2008-10-12 16:48:14 -0400

    Started index_gem
    See notes on paper.  Decided to start with static file to avoid link issues.
    Decided on javascript data structures.

--- script/find_gems.rb
+++ script/find_gems.rb
@@ -1,13 +1,37 @@
 require "rubygems"
+require "hpricot"
 
-def find_doc_root
-  puts Gem.source_index.search(nil)[0].methods
-end
-
-#find_doc_root
 $rdoc_root = "/Library/Ruby/Gems/1.8/doc"
-Gem.source_index.search(nil).each do |spec|
-  rdoc_dir = File.join($rdoc_root, "#{spec.name}-#{spec.version}")
-  
-  puts "#{spec.name} #{spec.version} #{spec.has_rdoc? ? "rdoc" : ""} #{File.exists?(rdoc_dir) ? "exists" : ""}"
+
+# Print installed gems and whether rdocs are available and indeed installed.
+def print_gems
+  Gem.source_index.search(nil).each do |spec|
+    puts "#{spec.name} #{spec.version} #{spec.has_rdoc? ? "has rdoc" : ""} #{File.exists?(rdoc_dir) ? "and it exists" : "but it doesn't exist"}"
+  end
 end
+
+# Index a gem.
+def index_gem(spec)
+  rdoc_dir = File.join($rdoc_root, "#{spec.name}-#{spec.version}", "rdoc")
+  unless File.exists?(rdoc_dir)
+    puts "WARN Couldn't find rdoc dir #{rdoc_dir}"
+    return
+  end
+
+  methods_file = File.join(rdoc_dir, "fr_method_index.html")
+  unless File.exists?(methods_file)
+    puts "ERROR Couldn't find #{methods_file}"
+  end
+
+  classes_file = File.join(rdoc_dir, "fr_class_index.html")
+  unless File.exists?(classes_file)
+    puts "ERROR Couldn't find #{classes_file}"
+  end
+
+  doc = Hpricot(open(methods_file))
+puts doc.search("#index-entries")
+end
+
+# mainline
+
+index_gem(Gem.source_index.search(nil)[0])
