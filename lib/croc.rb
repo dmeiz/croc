6e801eb257acef9d75c4a054e3f2330ddfe7b67b
Dan Hensgen <dan@macbook.(none)>
2008-11-07 22:41:13 -0500

    Removed index_gem and used index_rdoc

--- lib/croc.rb
+++ lib/croc.rb
@@ -50,60 +50,6 @@ def get_specs
   specs.values
 end
 
-# Index a gem.
-def index_gem(spec)
-  rdoc_dir = File.join($rdoc_home, "#{spec.name}-#{spec.version}", "rdoc")
-  unless File.exists?(rdoc_dir)
-    puts "WARN Couldn't find rdoc dir #{rdoc_dir}"
-    return
-  end
-
-  methods_file = File.join(rdoc_dir, "fr_method_index.html")
-  unless File.exists?(methods_file)
-    puts "ERROR Couldn't find #{methods_file}"
-  end
-
-  classes_file = File.join(rdoc_dir, "fr_class_index.html")
-  unless File.exists?(classes_file)
-    puts "ERROR Couldn't find #{classes_file}"
-  end
-
-  # collect gem
-  gem = {:name => spec.name, :dir => rdoc_dir, :classes => {}}
-  $gems << gem
-
-  # collect classes
-  doc = Hpricot(open(classes_file))
-  (doc/"#index-entries a").each do |el|
-    klass = el.inner_html
-  
-    if $classes[klass]
-      puts "Already have class #{klass}"
-    else
-      gem[:classes][klass] = {:name => klass, :url => el["href"], :methods => []}
-    end
-  end
-
-  # collect methods
-  doc = Hpricot(open(methods_file))
-  (doc/"#index-entries a").each do |el|
-    if el.inner_html =~ /(\S+)\s+\((\S+)\)/
-      method = $1
-      if klass = gem[:classes][$2]
-        el["href"] =~ /#(.*)$/
-        url = $1
-        klass[:methods] << {:name => method, :class => klass, :url => url}
-      else
-        # puts %Q(Couldn't find class "#{$2} for method #{method}")
-      end
-    else
-      # puts %Q(Couldn't get method and class from "#{el.inner_html}")
-    end
-  end
-
-  puts "Indexed #{spec.name}"
-end
-
 def index_rdoc(name, rdoc_dir)
   unless File.exists?(rdoc_dir)
     puts "WARN Couldn't find rdoc dir #{rdoc_dir}"
