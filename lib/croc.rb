5bddd45183c56dab174b40b3ec7bf06066179287
Dan Hensgen <dan@macbook.(none)>
2008-11-07 22:38:01 -0500

    Index files under .croc/rdoc

--- lib/croc.rb
+++ lib/croc.rb
@@ -19,6 +19,7 @@ def install_assets
   croc_dir = File.join(user_home_dir, ".croc")
   unless File.exists?(croc_dir)
     Dir.mkdir(croc_dir)
+    Dir.mkdir(File.join(croc_dir, "rdoc"))
   end
 
   public_dir = File.join(File.dirname(__FILE__), "..", "public")
@@ -101,4 +102,56 @@ def index_gem(spec)
   end
 
   puts "Indexed #{spec.name}"
+end
+
+def index_rdoc(name, rdoc_dir)
+  unless File.exists?(rdoc_dir)
+    puts "WARN Couldn't find rdoc dir #{rdoc_dir}"
+    return
+  end
+
+  methods_file = File.join(rdoc_dir, "fr_method_index.html")
+  unless File.exists?(methods_file)
+    puts "ERROR Couldn't find #{methods_file}"
+  end
+
+  classes_file = File.join(rdoc_dir, "fr_class_index.html")
+  unless File.exists?(classes_file)
+    puts "ERROR Couldn't find #{classes_file}"
+  end
+
+  # collect gem
+  gem = {:name => name, :dir => rdoc_dir, :classes => {}}
+  $gems << gem
+
+  # collect classes
+  doc = Hpricot(open(classes_file))
+  (doc/"#index-entries a").each do |el|
+    klass = el.inner_html
+  
+    if $classes[klass]
+      puts "Already have class #{klass}"
+    else
+      gem[:classes][klass] = {:name => klass, :url => el["href"], :methods => []}
+    end
+  end
+
+  # collect methods
+  doc = Hpricot(open(methods_file))
+  (doc/"#index-entries a").each do |el|
+    if el.inner_html =~ /(\S+)\s+\((\S+)\)/
+      method = $1
+      if klass = gem[:classes][$2]
+        el["href"] =~ /#(.*)$/
+        url = $1
+        klass[:methods] << {:name => method, :class => klass, :url => url}
+      else
+        # puts %Q(Couldn't find class "#{$2} for method #{method}")
+      end
+    else
+      # puts %Q(Couldn't get method and class from "#{el.inner_html}")
+    end
+  end
+
+  puts "Indexed #{name}"
 end
